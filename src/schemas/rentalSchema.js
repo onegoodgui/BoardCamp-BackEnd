004d34ebfd2e40c3c18e1397db7450eb27ed630c
Adriano <adrianogotardo.12@gmail.com>
2022-03-10 18:30:09 -0300

    Preparando review

--- src/schemas/rentalSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-
-const rentalSchema = joi.object({
-    customerId: joi.number().positive().required(),
-    gameId: joi.number().positive().required(),
-    daysRented: joi.number().positive().required(),
-  });
-
-  export default rentalSchema
